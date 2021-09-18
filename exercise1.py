9709ba95a60daa4f4292b2fe0fec239f771f6e18
jvetere-jama <84156757+jvetere-jama@users.noreply.github.com>
2021-09-18 16:05:24 -0700

    Building exercise1.py file.

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+'''
+Lab 1: Introduction to Python for Embedded CPS
+Student Name: Joe Vetere
+MSU NetID: veterejo
+
+This files prints "Hello World", contains two code blocks and comments.
+'''
+myStr = "Hello World"
+print(myStr)
