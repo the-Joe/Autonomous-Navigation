ddfe0275f3fc1780c138d4628534c947d50137d9
the-Joe <joe@vetere.us>
2021-09-18 20:47:08 -0700

    Finalizing exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -150,3 +150,140 @@ result = print_list([100,200,300])
 
 print(result)
 
+
+'''
+Slope of line can be calculated by:
+m = rise/run = (y2 - y1)/(x2 - x1)
+
+Line of best fit formula:
+y = mx + b
+
+Converted to solve for b
+b = y1 - (m * x1)
+'''
+
+def findLine(x1, y1, x2, y2):
+	#Slope of line
+	m = (y2 - y1)/(x2 - x1)
+
+	#Line of best fit
+	b = y1 - (m * x1)
+
+	#Format values to two decimals
+	m = round(m,2)
+	b = round(b,2)
+	return m, b
+
+m, b = findLine(4, 8, 10, 3)
+print(m, b) #should return -0.83, 11.3
+
+m, b = findLine(1.2, 1.4, 8.7, 7.2)
+print(m, b) #should return 0.77, 0.47
+
+#%% Step 7
+print("========= Step 7 ========= ")
+
+myDictionary = {} #defining an empty dictionary
+
+#defining a dictionary with items 
+#keys can be a string or integer
+myDictionary = {
+	"key1": "hello",
+	"key2": "world"
+}
+
+print(myDictionary)
+print(myDictionary["key1"]) #print out the value at "key1"
+
+#you can change the value of a key in the dictionary with the below syntax
+myDictionary["key1"] = "world"
+
+#you can define a new key in the dictionary with the below syntax
+myDictionary["key3"] = 123
+
+print(myDictionary)
+
+#%% Step 8
+print("========= Step 8 ========= ")
+
+
+"""
+Grade Calculator Program
+"""
+
+#The main function containing the program code
+#accepts a student’s records as list of dictionaries
+#and will output the final course grade of the student
+#in the form of a float (e.g., 92.67)
+def calculateMyGrade(studentGradeslist):
+
+	#first define the weight given to diff items
+	GRADING = {
+		"Exam": 0.6,
+		"Homework": 0.1,
+		"Project": 0.3
+	}
+
+	pointsScored = {"Exam": [], "Homework": [], "Project": []} #track the points the student got
+	pointsPossible = {"Exam": [], "Homework": [], "Project": []} #track the points possible
+
+	#loop through the student’s grades
+	for i in studentGradeslist:
+		if i["ItemType"] == "Exam":
+			pointsScored["Exam"].append(i["Score"])
+			pointsPossible["Exam"].append(i["MaximumPossibleScore"])
+		elif i["ItemType"] == "Homework":
+			pointsScored["Homework"].append(i["Score"])
+			pointsPossible["Homework"].append(i["MaximumPossibleScore"])
+		else:
+			pointsScored["Project"].append(i["Score"])
+			pointsPossible["Project"].append(i["MaximumPossibleScore"])
+
+	
+	#Sum the exam scores using sum()
+	examWeightedScore = (GRADING["Exam"]*sum(pointsScored["Exam"])/float(sum(pointsPossible["Exam"])))
+	
+	#Remove dropped assignment from pointsScored and pointsPossible. Verifies value no longer exists and notifies student.
+	pointsScored["Homework"].remove(0)
+	print("Removed dropped homework assignment from total homework points scored. Scores recorded are: " + str(pointsScored["Homework"]))
+	pointsPossible["Homework"].remove(0)
+	print("Removed dropped homework assignment from total homework points possible. Possible points recorded are:" + str(pointsPossible["Homework"]))
+	
+	#Sum the homework scores using sum()
+	homeworkWeightedScore = (GRADING["Homework"]*sum(pointsScored["Homework"])/float(sum(pointsPossible["Homework"])))
+	
+	#Sum the project scores using sum()
+	projectWeightedScore = (GRADING["Project"]*sum(pointsScored["Project"])/float(sum(pointsPossible["Project"])))
+
+	return examWeightedScore + homeworkWeightedScore + projectWeightedScore
+
+#Find out a student’s grade by calling the function
+student1Grades = [
+	{"ItemType": "Exam",
+	"Score": 92.55,
+	"MaximumPossibleScore": 100},
+	{"ItemType": "Exam",
+	"Score": 72.15,
+	"MaximumPossibleScore": 86},
+	{"ItemType": "Homework",
+	"Score": 32,
+	"MaximumPossibleScore": 100},
+	{"ItemType": "Homework",
+	"Score": 0,
+	"MaximumPossibleScore": 0},
+	{"ItemType": "Homework",
+	"Score": 93.122,
+	"MaximumPossibleScore": 95},
+	{"ItemType": "Project",
+	"Score": 113,
+	"MaximumPossibleScore": 157},
+	{"ItemType": "Project",
+	"Score": 176,
+	"MaximumPossibleScore": 179}
+]
+
+#when printing the final grade, we round the results using
+#the round() function, and then multiply by 100 to get a percentage
+print("The final grade is: " + str(100*round(calculateMyGrade(student1Grades), 5) ))
+
+
